801c3c755c6e54d7edf6f3c04e6a0b07f26e6372
sufian <sufian@brainstation-23.com>
2017-01-04 15:32:58 +0600

    startup url modified for command line and iis.

--- src/CoreWebAPI/Program.cs
+++ src/CoreWebAPI/Program.cs
@@ -14,6 +14,7 @@ namespace CoreWebAPI
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:5001")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
