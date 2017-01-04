38b54f03da96e2e76fb195d88fa445f5d2b44d82
sufian <sufian@brainstation-23.com>
2017-01-04 16:34:24 +0600

    console api client added.

---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var r = Run().Result;
+        }
+
+        private static async Task<bool> Run()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return false;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            return true;
+        }
+    }
+}
